2bcbb49a453fa124748349c635500f8bddfe466d
hamdi <hamdi.pirillo4@gmail.com>
2024-09-29 09:08:06 +0100

    used the children prop to display the list of replies inside a comment

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -19,7 +19,7 @@ export default function CommentSection({
     const [userComments, setUserComments] = React.useState(comments)
 
     return (
-        <div className='grid gap-4'>
+        <div className='comments-section grid gap-4'>
             {userComments.map((comment, i) => {
                 const replies = comment.replies
 
@@ -38,7 +38,19 @@ export default function CommentSection({
                             ])))
                         }}
                         key={i}
-                    />
+                    >
+                        {replies?.length > 0 && (
+                            <div className='grid gap-4 p-4 pr-0 pb-0 ml-14'>
+                                {replies.map((reply, i) => (
+                                    <Comment
+                                        data={reply}
+                                        updateComment={() => alert('The reply to replies functionality is currently being developed.')}
+                                        key={i}
+                                    />
+                                ))}
+                            </div>
+                        )}
+                    </Comment>
                 )})
             }
 
