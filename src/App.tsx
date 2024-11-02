d8704de5d79721434f37e25eacfd58355027b566
hamdi <hamdi.pirillo4@gmail.com>
2024-11-02 15:37:58 +0100

    saving changes before making experimental branch

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ export const reducer = (state: UserComment[], action: {
         behavior: 'smooth'
       }), 0)
 
-      return updateLocalStorage([...state, newComment])
+      return updateLocalStorage([newComment, ...state])
 
     case 'REPLY_COMMENT': {
       const userComment = action.comment!
